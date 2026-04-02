ae291eebfb1b96930f4156d33fb06bd8a74de497
GGFAHRIN <fahrinmuneer3@gmail.com>
2026-04-02 10:08:05 +0800

    half

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 
 def compare_averages(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    math_avg = float(round(df["Math"].mean(), 1))
+    Science_avg = float(round(df["Science"].mean(), 1))
+    English_avg = float(round(df["English"].mean(), 1))
+
+    
+
+compare_averages("labs/lab09/data/students.csv")
